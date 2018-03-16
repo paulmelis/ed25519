43e8f78b415fe9c9a440d03bf62fc5bccd5c1225
Paul Melis <paulmelis@gmail.com>
2018-03-16 21:08:41 +0100

    Basic test fixes

--- test/basic.py
+++ test/basic.py
@@ -2,11 +2,14 @@
 import ed25519
 
 message = b'Hello world!'
+seed = b'abcdefghijklmnopqrstuvwxyz789012'
 
-pubkey, privkey = ed25519.create_keypair(b'abcdefghijklmnopqrstuvwxyz789012')
+pubkey, privkey = ed25519.create_keypair(seed)
+
+derived_pubkey = ed25519.get_pubkey(privkey)
+assert derived_pubkey == pubkey
 
 signature = ed25519.sign(message, pubkey, privkey)
-print(signature)
 assert len(signature) == 64
 
 res = ed25519.verify(signature, message, pubkey)
@@ -15,4 +18,3 @@ assert res
 signature = bytes([255 - signature[0]]) + signature[1:]
 res = ed25519.verify(signature, message, pubkey)
 assert not res
-
